4b81653911f15d08bbc55d1f47ef42caef65a1ad
Vaidas Datenis <datenis.v@gmail.com>
2020-03-11 12:10:23 +0200

    Successfully added data to Firebase. Need to work with authentication and rules in database.

--- scrape.py
+++ scrape.py
@@ -3,56 +3,78 @@ from bs4 import BeautifulSoup
 import json
 from requests_html import HTMLSession
 import re
+from firebase import firebase
 
-session = HTMLSession()
+# authentication = firebase.Authentication(
+#     'THIS_IS_MY_SECRET', 
+#     'datenis.v@gmail.com', 
+#     extra={'id': 123}
+# )
+# firebase.authentication = authentication
 
-data = []
-remove = [",", "Kitas", "Bet", "kuris", "miestas", "\u20ac"]
+# session = HTMLSession()
 
-def removeUnnessery(var):
-    edited_string = var.split()
-    final_list = [word for word in edited_string if word not in remove]
-    var = ' '.join(final_list)
-    return var
+# data = []
+# remove = [",", "Kitas", "Bet", "kuris", "miestas", "\u20ac"]
 
-for num in range(1, 100):
-    page = str(num)
-    url = "https://www.cvbankas.lt/?page="+page
-    print(url)
-    responce = session.get(url)
-    if responce is not None:
-        soup = BeautifulSoup(responce.content, 'html.parser')
-        results = soup.find('div', id='job_ad_list')
+# def removeUnnessery(var):
+#     edited_string = var.split()
+#     final_list = [word for word in edited_string if word not in remove]
+#     var = ' '.join(final_list)
+#     return var
 
-        job_elems = results.find_all('article', class_='list_article list_article_rememberable jobadlist_list_article_rememberable jobadlist_article_vip')
-        for job_elem in job_elems:
-            title_elem = job_elem.find('h3', class_='list_h3')
-            company_elem = job_elem.find('span', class_='dib mt5')
-            salary_elem = job_elem.find('span', class_='salary_amount')
-            location_elem = job_elem.find('span', class_='list_city')
+# for num in range(1, 100):
+#     page = str(num)
+#     url = "https://www.cvbankas.lt/?page="+page
+#     print(url)
+#     responce = session.get(url)
+#     if responce is not None:
+#         soup = BeautifulSoup(responce.content, 'html.parser')
+#         results = soup.find('div', id='job_ad_list')
 
-            address = job_elem.find('a', class_="list_a can_visited list_a_has_logo", href=True)
-            request_address = requests.get(address['href'])
-            soup2 = BeautifulSoup(request_address.text, 'html.parser')
-            job_address = soup2.find('a', class_='partners_company_info_additional_info_location_url')
+#         job_elems = results.find_all('article', class_='list_article list_article_rememberable jobadlist_list_article_rememberable jobadlist_article_vip')
+#         for job_elem in job_elems:
+#             title_elem = job_elem.find('h3', class_='list_h3')
+#             company_elem = job_elem.find('span', class_='dib mt5')
+#             salary_elem = job_elem.find('span', class_='salary_amount')
+#             location_elem = job_elem.find('span', class_='list_city')
 
-            if None in (title_elem, company_elem, salary_elem, location_elem, job_address):
-                continue
-            title = title_elem.text.strip()
-            company = company_elem.text.strip()
-            salary = salary_elem.get_text()
-            location = location_elem.text.strip()
-            city_location = job_address.get_text()
-            removeUnnessery(salary)
-            data.append(
-                {
-                    "title": title, 
-                    "company": company, 
-                    "salary": salary, 
-                    "location": location, 
-                    "address": city_location
-                }
-            )
+#             address = job_elem.find('a', class_="list_a can_visited list_a_has_logo", href=True)
+#             request_address = requests.get(address['href'])
+#             soup2 = BeautifulSoup(request_address.text, 'html.parser')
+#             job_address = soup2.find('a', class_='partners_company_info_additional_info_location_url')
 
-with open("job-list.json", "w", encoding='utf-8') as write_file:
-    json.dump(data, write_file, ensure_ascii=False, indent=4)
+#             if None in (title_elem, company_elem, salary_elem, location_elem, job_address):
+#                 continue
+#             title = title_elem.text.strip()
+#             company = company_elem.text.strip()
+#             salary = salary_elem.get_text()
+#             location = location_elem.text.strip()
+#             city_location = job_address.get_text()
+#             removeUnnessery(salary)
+#             data.append(
+#                 {
+#                     "title": title, 
+#                     "company": company, 
+#                     "salary": salary, 
+#                     "location": location, 
+#                     "address": city_location
+#                 }
+#             )
+
+firebase = firebase.FirebaseApplication(
+    'https://ns-ng-challenge-app.firebaseio.com/', 
+    None
+)
+new_job = {
+        "title": "Administratorius (-ė) - registratorius (-ė)",
+        "company": "UAB Sveikatos centras „Energetikas“",
+        "salary": "550-650",
+        "location": "Palangoje",
+        "address": "Kuršių takas 1, LT-00317 Palanga"
+    }
+result = firebase.post('/jobs', new_job)
+print("===== "+result+" =====")
+
+# with open("job-list.json", "w", encoding='utf-8') as write_file:
+#     json.dump(data, write_file, ensure_ascii=False, indent=4)
